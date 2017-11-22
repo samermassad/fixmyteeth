a6abbf5d39518422a7f9811bb88f46a624dc8ef5
samermassad <33847166+samermassad@users.noreply.github.com>
2017-11-22 22:59:14 +0100

    this is samer

--- functions.php
+++ functions.php
@@ -1,14 +1,32 @@
 <?php
+function db_connect() {
+    $servername = "localhost";
+    $username = "root";
+    $password = "fixmyteeth";
 
-$servername = "localhost";
-$username = "root";
-$password = "fixmyteeth";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, "fixmyteeth");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    return $conn;
+}
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
+function search($conn, $address, $city, $specialty, $name) {
+    $query = "SELECT * FROM dentists WHERE ";
+    $query .= empty($address) ? "" : "address = '$address' AND ";
+    $query .= empty($city) ? "" : "city = '$city' AND ";
+    $query .= empty($specialty) ? "" : "specialty = '$specialty' AND ";
+    $query .= empty($name) ? "" : "CONCAT( first_name,  ' ', last_name ) LIKE  '%$name%';";
+    $results = mysqli_query($conn, $query);
+    $return = array();
+    foreach(mysqli_fetch_all($results) as $key => $row) {
+        $return[$key] = array();
+        foreach($row as $field) {
+            $return[$key][] = htmlspecialchars($field);
+        }
+    }
+    return $return;
+}
--- index.php
+++ index.php
@@ -1,13 +1,66 @@
 <!DOCTYPE html>
+<?php
+include 'functions.php';
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
+        <form method="post">
+            <input type="text" name="address">
+            <input type="text" name="city">
+            <input type="text" name="specialty">
+            <input type="text" name="name">
+            <input type="submit" name="submit">
+        </form>
         <?php
-        echo "this is a test";
-	echo "this is a test Stefano";
+            if(isset($_POST['submit'])) {
+                $address = $_POST['address'];
+                $city = $_POST['city'];
+                $specialty = $_POST['specialty'];
+                $name = $_POST['name'];
+                
+                $conn = db_connect();
+                
+                $results = search($conn, $address, $city, $specialty, $name);
+                
+                ?>
+        <table id="results">
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+                <th>Address</th>
+                <th>City</th>
+                <th>Phone</th>
+                <th>Image</th>
+                <th>Hours</th>
+                <th>Specialty</th>
+            </tr>
+            <?php 
+            foreach($results as $key => $value) {
+                ?>
+            <tr>
+                <th><?php echo $value[1]; ?></th>
+                <th><?php echo $value[2]; ?></th>
+                <th><?php echo $value[3]; ?></th>
+                <th><?php echo $value[4]; ?></th>
+                <th><?php echo $value[5]; ?></th>
+                <th><?php echo $value[6]; ?></th>
+                <th><?php echo $value[7]; ?></th>
+                <th><?php echo $value[8]; ?></th>
+                <th><?php echo $value[9]; ?></th>
+                <th><?php echo $value[10]; ?></th>
+            </tr>
+            <?php
+            }
+            ?>
+        </table>
+        <?php
+            }
         ?>
     </body>
 </html>
