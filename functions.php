b0604c56b25e21770fc7e4171bf463511b62b25c
alvarobilbao <bilbialvaro@gmail.com>
2017-11-23 15:48:40 -0400

    add to

--- functions.php
+++ functions.php
@@ -127,7 +127,7 @@ function display_search_bar() {
     <td>
     <div class="group">
      <input from="from" type="text" list="browser0"/><span class="highlight"></span><span class="bar"></span>
-     <label>Time Schedule(24h format)</label>
+     <label>Start time</label>
         <datalist id="browser0">
  		<option value="00:00">
   		<option value="00:30">
@@ -181,6 +181,63 @@ function display_search_bar() {
 		</datalist>
     </div>
     </td>
+    <td>
+        <div class="group">
+         <input from="to" type="text" list="browser0"/><span class="highlight"></span><span class="bar"></span>
+         <label>End time</label>
+            <datalist id="browser0">
+     		<option value="00:00">
+      		<option value="00:30">
+     		<option value="01:00">
+      		<option value="01:30">
+     		<option value="02:00">
+      		<option value="02:30">
+            <option value="03:00">
+      		<option value="03:30">
+     		<option value="04:00">
+      		<option value="04:30">
+     		<option value="05:00">
+      		<option value="05:30">
+            <option value="06:00">
+      		<option value="06:30">
+     		<option value="07:00">
+      		<option value="07:30">
+     		<option value="08:00">
+      		<option value="08:30">
+            <option value="09:00">
+      		<option value="09:30">
+     		<option value="10:00">
+      		<option value="10:30">
+     		<option value="11:00">
+      		<option value="11:30">
+            <option value="12:00">
+      		<option value="12:30">
+     		<option value="13:00">
+      		<option value="13:30">
+     		<option value="14:00">
+      		<option value="14:30">
+            <option value="15:00">
+      		<option value="15:30">
+     		<option value="16:00">
+      		<option value="16:30">
+     		<option value="17:00">
+      		<option value="17:30">
+            <option value="18:00">
+      		<option value="18:30">
+     		<option value="19:00">
+      		<option value="19:30">
+     		<option value="20:00">
+      		<option value="20:30">
+            <option value="21:00">
+      		<option value="21:30">
+     		<option value="22:00">
+      		<option value="22:30">
+     		<option value="23:00">
+      		<option value="23:30">
+            <option value="Not to specify">
+    		</datalist>
+        </div>
+        </td>
     <td>
     <div class="group">
      <input name="gender" type="text" list="browser2"/><span class="highlight"></span><span class="bar"></span>
