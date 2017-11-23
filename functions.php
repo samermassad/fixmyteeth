bf12243ebe4305f085eb4518598c9b0624227832
samermassad <33847166+samermassad@users.noreply.github.com>
2017-11-23 15:15:37 +0100

    General Update

--- functions.php
+++ functions.php
@@ -10,13 +10,13 @@ function db_connect() {
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    } 
+    }
     return $conn;
 }
 
 function search($address, $city, $specialty, $name, $day, $fromto, $photo, $gender) {
     $conn = db_connect();
-    
+
     //create the query string
     $query = "SELECT * FROM `dentists` WHERE ";
     $query .= empty($address) ? "`address` LIKE '%' AND " : "CONCAT(`address`) LIKE  '%$address%' AND ";
@@ -26,7 +26,7 @@ function search($address, $city, $specialty, $name, $day, $fromto, $photo, $gend
     $query .= empty($photo) ? "`image` LIKE '%' AND " : "`image` != ' ' AND ";
     $query .= empty($gender) ? "`gender` LIKE '%'" : "`gender` = '$gender'";
     $query .= ";";
-    
+
     $results = mysqli_query($conn, $query);
     $return = array();
     foreach(mysqli_fetch_all($results) as $row) {
@@ -45,10 +45,10 @@ function check_hours($day, $fromto, $hours) {
     // check if the dentists is available in the searched period
     // returns true if available
     // returns false if not available
-    
+
     $from = strtotime($fromto[0]) ? strtotime($fromto[0]) : strtotime("23:59"); //check if defined or not and assign the values
     $to = strtotime($fromto[1]) ? strtotime($fromto[1]) : strtotime("00:00");
-    
+
     if(strcmp($day,"any") == 0) {
         //returns true at first match
         foreach($hours as $value) {
@@ -59,4 +59,142 @@ function check_hours($day, $fromto, $hours) {
         if($from >= strtotime($hours[$day]['open']) && $to <= strtotime($hours[$day]['close']))   return true;
     }
     return false;
+}
+
+function get_specilties() {
+    $conn = db_connect();
+    $result = mysqli_query($conn, "SELECT `specialty` FROM dentists; ");
+    $storeArray = Array();
+    while ($row = mysqli_fetch_array($result, true)) {
+        if(empty(trim($row['specialty'])))   $row['specialty'] = "General Dentist";
+        if(!in_array($row['specialty'], $storeArray)) $storeArray[] = $row['specialty'];
+    }
+    var_dump($storeArray);
+}
+
+function display_search_bar() {
+    ?>
+<form method="post" action="result.php">
+    <table>
+    <tr>
+    <th>
+    <div class="group">
+      <input type="text"/><span class="highlight"></span><span class="bar"></span>
+      <label>Locate Dentist (Address)</label>
+    </div>
+    </th>
+    <th>
+    <div class="group">
+      <input type="text"/><span class="highlight"></span><span class="bar"></span>
+      <label>Your City</label>
+    </div>
+    </th>
+    <th>
+    <div class="group">
+      <input type="text"/><span class="highlight"></span><span class="bar"></span>
+      <label>Specialty</label>
+    </div>
+    </th>
+    <th>
+    <div class="group">
+      <input type="text"/><span class="highlight"></span><span class="bar"></span>
+      <label>Doctor Name</label>
+    </div>
+    </th>
+    </tr>
+    <tr>
+    <td>
+    <div class="group">
+     <input type="text" list="browser1"/><span class="highlight"></span><span class="bar"></span>
+     <label>Day</label>
+        <datalist id="browser1">
+                <option value="Not to specify">
+ 		<option value="Monday">
+  		<option value="Tuesday">
+ 		<option value="Wednesday">
+  		<option value="Thursday">
+ 		<option value="Saturday">
+  		<option value="Sunday">
+	</datalist>
+    </div>
+    </td>
+    <td>
+    <div class="group">
+     <input type="text" list="browser0"/><span class="highlight"></span><span class="bar"></span>
+     <label>Time Schedule(24h format)</label>
+        <datalist id="browser0">
+            <option value="Not to specify">
+            <option value="00:00">
+            <option value="00:30">
+            <option value="01:00">
+            <option value="01:30">
+            <option value="02:00">
+            <option value="02:30">
+            <option value="03:00">
+            <option value="03:30">
+            <option value="04:00">
+            <option value="04:30">
+            <option value="05:00">
+            <option value="05:30">
+            <option value="06:00">
+            <option value="06:30">
+            <option value="07:00">
+            <option value="07:30">
+            <option value="08:00">
+            <option value="08:30">
+            <option value="09:00">
+            <option value="09:30">
+            <option value="10:00">
+            <option value="10:30">
+            <option value="11:00">
+            <option value="11:30">
+            <option value="12:00">
+            <option value="12:30">
+            <option value="13:00">
+            <option value="13:30">
+            <option value="14:00">
+            <option value="14:30">
+            <option value="15:00">
+            <option value="15:30">
+            <option value="16:00">
+            <option value="16:30">
+            <option value="17:00">
+            <option value="17:30">
+            <option value="18:00">
+            <option value="18:30">
+            <option value="19:00">
+            <option value="19:30">
+            <option value="20:00">
+            <option value="20:30">
+            <option value="21:00">
+            <option value="21:30">
+            <option value="22:00">
+            <option value="22:30">
+            <option value="23:00">
+            <option value="23:30">
+        </datalist>
+    </div>
+    </td>
+    <td>
+    <div class="group">
+     <input type="text" list="browser2"/><span class="highlight"></span><span class="bar"></span>
+     <label>Gender</label>
+        <datalist id="browser2">
+            <option value="Not to specify">
+            <option value="Female">
+            <option value="Male">
+	</datalist>
+    </div>
+    </td>
+    <td>
+    <div class="group">
+     <input type="text" list="browser3"/><span class="highlight"></span><span class="bar"></span>
+     <label>Photo?</label>
+     <input type="checkbox" value="Photo?" />
+    </div>
+    </td>
+    </tr>
+    </table>
+</form>
+<?php
 }
--- the_site/index.php
+++ the_site/index.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php 
+include('../functions.php');
+?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
@@ -12,12 +15,12 @@
 
 <body>
 
-	<div id="container">
-        <div id="content-slider">
-      <div id="slider">  <!-- Slider container -->
-         <div id="mask">  <!-- Mask -->
+	<div id="container" style="background: black;">
+<!--        <div id="content-slider">
+      <div id="slider">   Slider container 
+         <div id="mask">   Mask 
          <ul>
-         <li id="first" class="firstanimation">  <!-- ID for tooltip and class for animation -->
+         <li id="first" class="firstanimation">   ID for tooltip and class for animation 
          <img src="web_elements/background.jpg" alt="Cougar"/>
          </li>
          <li id="second" class="secondanimation">
@@ -30,130 +33,11 @@
          <img src="web_elements/background_00.jpg" alt="Msg"/>
          </li>
          </ul>
-         </div>  <!-- End Mask -->
-         <div class="progress-bar"></div>  <!-- Progress Bar -->
-      </div>  <!-- End Slider Container -->
-    </div>
-    <table>
-    <tr>
-    <th>
-    <div class="group">
-      <input type="text"/><span class="highlight"></span><span class="bar"></span>
-      <label>Locate Dentist (Address)</label>
-    </div>
-    </th>
-    <th>
-    <div class="group">
-      <input type="text"/><span class="highlight"></span><span class="bar"></span>
-      <label>Your City</label>
-    </div>
-    </th>
-    <th>
-    <div class="group">
-      <input type="text"/><span class="highlight"></span><span class="bar"></span>
-      <label>Specialty</label>
-    </div>
-    </th>
-    <th>
-    <div class="group">
-      <input type="text"/><span class="highlight"></span><span class="bar"></span>
-      <label>Doctor Name</label>
-    </div>
-    </th>
-    </tr>
-    <tr>
-    <td>
-    <div class="group">
-     <input type="text" list="browser1"/><span class="highlight"></span><span class="bar"></span>
-     <label>Day</label>
-        <datalist id="browser1">
-                <option value="Not to specify">
- 		<option value="Monday">
-  		<option value="Tuesday">
- 		<option value="Wednesday">
-  		<option value="Thursday">
- 		<option value="Saturday">
-  		<option value="Sunday">
-	</datalist>
-    </div>
-    </td>
-    <td>
-    <div class="group">
-     <input type="text" list="browser0"/><span class="highlight"></span><span class="bar"></span>
-     <label>Time Schedule(24h format)</label>
-        <datalist id="browser0">
-            <option value="Not to specify">
-            <option value="00:00">
-            <option value="00:30">
-            <option value="01:00">
-            <option value="01:30">
-            <option value="02:00">
-            <option value="02:30">
-            <option value="03:00">
-            <option value="03:30">
-            <option value="04:00">
-            <option value="04:30">
-            <option value="05:00">
-            <option value="05:30">
-            <option value="06:00">
-            <option value="06:30">
-            <option value="07:00">
-            <option value="07:30">
-            <option value="08:00">
-            <option value="08:30">
-            <option value="09:00">
-            <option value="09:30">
-            <option value="10:00">
-            <option value="10:30">
-            <option value="11:00">
-            <option value="11:30">
-            <option value="12:00">
-            <option value="12:30">
-            <option value="13:00">
-            <option value="13:30">
-            <option value="14:00">
-            <option value="14:30">
-            <option value="15:00">
-            <option value="15:30">
-            <option value="16:00">
-            <option value="16:30">
-            <option value="17:00">
-            <option value="17:30">
-            <option value="18:00">
-            <option value="18:30">
-            <option value="19:00">
-            <option value="19:30">
-            <option value="20:00">
-            <option value="20:30">
-            <option value="21:00">
-            <option value="21:30">
-            <option value="22:00">
-            <option value="22:30">
-            <option value="23:00">
-            <option value="23:30">
-        </datalist>
-    </div>
-    </td>
-    <td>
-    <div class="group">
-     <input type="text" list="browser2"/><span class="highlight"></span><span class="bar"></span>
-     <label>Gender</label>
-        <datalist id="browser2">
-            <option value="Not to specify">
-            <option value="Female">
-            <option value="Male">
-	</datalist>
-    </div>
-    </td>
-    <td>
-    <div class="group">
-     <input type="text" list="browser3"/><span class="highlight"></span><span class="bar"></span>
-     <label>Photo?</label>
-     <input type="checkbox" value="Photo?" />
-    </div>
-    </td>
-    </tr>
-    </table>
+         </div>   End Mask 
+         <div class="progress-bar"></div>   Progress Bar 
+      </div>   End Slider Container 
+    </div>-->
+    <?php display_search_bar(); ?>
 	</div>
 
 </body>
