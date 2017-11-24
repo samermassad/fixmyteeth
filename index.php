cc748f69a46569cd00ddb406642b195ca4a1aa1f
alvarobilbao <bilbialvaro@gmail.com>
2017-11-24 09:57:53 -0400

    revert

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include 'functions.php';
 ?>
 <html>
     <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <link rel= "stylesheet" type="text/css" href="index_style.css" />
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
             crossorigin="anonymous" ></script>
