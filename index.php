d577189d701efab8b9aeeea925217b4e1be8e518
samermassad <33847166+samermassad@users.noreply.github.com>
2017-11-24 15:24:12 +0100

    bubble

--- index.php
+++ index.php
@@ -27,6 +27,9 @@ include 'functions.php';
             <div class="welcome2"><p>4 Environment</p></div>
     <!--        <div class="arrow-down"></div>-->
             <div class="teeth"><img onclick="submit_form();" src="web_elements/teeth.png" width="100px" height="100px"/></img></div>
+            <div class="bubble">
+                <input style="font-size:0.01em;height:25px;width:25px;border:none" type="submit" value="">
+            </div>
         </div>
         <div class="container" id="container">
             <?php
